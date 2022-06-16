73802230d20ba68f740424aeb3b137e801a20497
Serghei Carpov <serghei.carpov@isa.utm.md>
2022-06-16 17:26:28 +0300

    configerate prettier-eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-test';
+  myVariable = '';
+  hello = '';
+  hello1 = 14;
+  hello3 = 78;
+
+  setHello() {
+    this.hello = 'hello';
+  }
 }
