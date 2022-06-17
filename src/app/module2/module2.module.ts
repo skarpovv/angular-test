4a40cf61b03a42df7752a328f16d67a27bb00012
Serghei Carpov <serghei.carpov@isa.utm.md>
2022-06-17 18:52:03 +0300

    add orderThings Pipe for module 2

--- src/app/module2/module2.module.ts
+++ src/app/module2/module2.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { Module2AreaComponent } from './module2-area/module2-area.component';
 import { Module2ThingComponent } from './module2-thing/module2-thing.component';
 import { SharingModule } from '../shared/sharing.module';
+import { OrderThingsPipe } from '../shared/pipes/order-things.pipe';
 
 const routes: Routes = [
   {
@@ -18,6 +19,7 @@ const routes: Routes = [
     Module2MainComponent,
     Module2AreaComponent,
     Module2ThingComponent,
+    OrderThingsPipe,
   ],
   imports: [SharingModule, CommonModule, RouterModule.forChild(routes)],
 })
